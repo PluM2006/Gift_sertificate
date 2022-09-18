79f918328acb9aacc01ec27fd6dc5988466f1bad
PluM2006 <plum2006@tut.by>
2022-09-18 17:07:35 +0300

    init commit

---
+++ src/main/java/ru/clevertec/gift_certificates/GiftCertificatesApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.gift_certificates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCertificatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftCertificatesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/clevertec/gift_certificates/GiftCertificatesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.gift_certificates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftCertificatesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
