8c04b1f5999d487724e5c4ff020409733df2131c
PluM2006 <PluM2006@tut.by>
2022-09-27 16:15:01 +0300

    mend

--- src/main/java/ru/clevertec/ecl/exception/NotFoundException.java
+++ src/main/java/ru/clevertec/ecl/exception/NotFoundException.java
@@ -4,9 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+import java.text.MessageFormat;
+
 @Getter
 @Setter
 public class NotFoundException extends RuntimeException {
+
     private String dtoName;
     private String fieldName;
     private Object fieldValue;
@@ -14,7 +17,7 @@ public class NotFoundException extends RuntimeException {
     private Integer errorCode;
 
     public NotFoundException(String dtoName, String fieldName, Object fieldValue, HttpStatus httpStatus, Integer errorCode) {
-        super(String.format("%s is not found with %s = %s", dtoName, fieldName, fieldValue));
+        super(MessageFormat.format("{0} is not found with {1} = {2}", dtoName, fieldName, fieldValue));
         this.dtoName = dtoName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
