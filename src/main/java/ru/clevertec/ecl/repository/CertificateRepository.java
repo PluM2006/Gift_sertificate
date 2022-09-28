4ec8c31ca30df735b9cca18e77a92c4c9eadd463
PluM2006 <plum2006@tut.by>
2022-09-28 22:55:51 +0300

    mend

--- src/main/java/ru/clevertec/ecl/repository/CertificateRepository.java
+++ src/main/java/ru/clevertec/ecl/repository/CertificateRepository.java
@@ -1,6 +1,7 @@
 package ru.clevertec.ecl.repository;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.clevertec.ecl.entity.Certificate;
@@ -10,12 +11,14 @@ import java.util.Optional;
 
 public interface CertificateRepository extends JpaRepository<Certificate, Long> {
 
+    @EntityGraph(attributePaths = {"tags"})
     Optional<Certificate> findByName(String name);
 
     @Query("SELECT c, t FROM Certificate c " +
             "LEFT JOIN c.tags t " +
             "WHERE (LOWER(c.description)  LIKE CONCAT('%',LOWER(:description),'%') or :description IS null )" +
             "AND (LOWER(t.name) = LOWER(:tagName) OR :tagName IS null)")
+    @EntityGraph(attributePaths = {"tags"})
     List<Certificate> findByTagNameDescription(String tagName, String description, Pageable pageable);
 
 }
